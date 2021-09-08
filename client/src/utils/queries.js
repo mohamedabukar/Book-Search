19770150d43f562a431fd94bfe6f006722e84674
Mohamed <mohamed.h.abukar@gmail.com>
2021-09-07 21:00:44 -0500

    files created and completed.

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+
+export const GET_ME = gql`
+{
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
